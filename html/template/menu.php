6c080c26d71ccef44ddc3a8fdbefd86e03361271
Yun SeokMin <elraine@naver.com>
2016-07-05 17:01:52 +0900

    index form

--- html/template/menu.php
+++ html/template/menu.php
@@ -1,9 +1,9 @@
 		
 <div id="menu">
 	<ul>
-		<li><a href="#"><h3>메뉴1</h3></a></li>
-		<li><a href="#"><h3>메뉴2</h3></a></li>
-		<li><a href="#"><h3>메뉴3</h3></a></li>
-		<li><a href="#"><h3>메뉴4</h3></a></li>
+		<li><a href="#"><h4>출결 조회</h4></a></li>
+		<li><a href="#"><h4>성적 조회</h4></a></li>
+		<li><a href="#"><h4>내 정보 보기</h4></a></li>
+		<li><a href="#"><h4>학생 검색<br>(교사 전용)</h4></a></li>
 	</ul>
 </div>

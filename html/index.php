2b02b0bd5c537942b1b2197cbf0c22bec2afe01b
Yun SeokMin <elraine@naver.com>
2016-07-05 10:24:24 +0900

    index form css

--- html/index.php
+++ html/index.php
@@ -1,6 +1,104 @@
-<?php
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<html>
 
-	echo "<h1>Welcome to Sturec</h1>";
+<head>
+	<title>Hello, World!</title>
+  
+</head>
+<style type="text/css">
 
+	.wrap{
+		margin: 0 auto;
+		padding: 0 auto;
+		
+	}
 
-?>
+	#div_loginbar{
+		height:35px;
+		background-color: blue;
+	}
+	
+	#header{
+		height:90px;
+		background-color: aqua;	
+	}
+
+	#footer{
+		height:100px;
+		background-color: aqua;	
+	}
+	
+	.content_wrap{
+		height: 800px;
+		margin: 0px 0px 10px 0px;
+	}
+	
+	#menu{
+		height: 100%;
+		width: 10%;
+		float: left;
+		background-color: green;
+		margin: 5px 5px 5px 5px;
+		margin-left: 5%;
+	}
+	
+	#content{
+		height: 100%;
+		width: 70%;
+		float:left;
+		background-color: skyblue;
+		margin: 5px 5px 5px 5px;
+		margin-right: 5%;
+		
+	}
+	
+	h1{
+		align-text: center;
+	}
+	
+	li{
+		height:50px;
+	}
+	
+</style>
+<body>  
+
+	<div class="wrap">
+
+		<div id="div_loginbar">
+		
+		</div>
+		
+		<div id="header">
+			<hr><h1>Welcome to StuRec!</h1><hr>
+		</div>
+		
+		<div class="content_wrap">		
+			
+			<div id="menu">
+				<ul>
+					<li><h3>메뉴1</h3></li>
+					<li><h3>메뉴2</h3></li>
+					<li><h3>메뉴3</h3></li>
+					<li><h3>메뉴4</h3></li>
+				</ul>
+				
+				
+			</div>
+			
+			<div id="content">
+			
+			</div>
+			
+		</div>
+		
+		<div id="footer">
+		
+		</div>
+	</div>
+</body>
+
+  
+
+</html>

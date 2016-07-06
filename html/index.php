1bf59eda16f3a93cd1a0bcf6cfe36d192cc98c9c
Yun SeokMin <elraine@naver.com>
2016-07-06 11:45:15 +0900

    add document root path

--- html/index.php
+++ html/index.php
@@ -5,29 +5,33 @@
 <head>
 	<title>Hello, Sturec!</title>
 	<link rel="stylesheet" type="text/css" href="/style/css/mystyle.css">
+
 </head>
+
 <body>  
 
 	<div class="wrap">
 	<?php 
-		require_once("/template/loginbar.php"); 
-		require_once("/template/header.php");
+		$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+		require_once($DOCUMENT_ROOT . "/template/loginbar.php"); 
+		require_once($DOCUMENT_ROOT . "/template/header.php");
 	?>
 		
 		<div class="content_wrap">		
 		<?php 
-			require_once("/template/menu.php");
+			require_once($DOCUMENT_ROOT . "/template/menu.php");
 		?>		
-			
+		
 			<div id="content">
 				<h1> 내용 </h1><br>
 				여기다 뭐 넣을까.
+				
 			</div>
 			
 		</div>
 		
 	<?php
-		require_once("/template/footer.php");
+		require_once($DOCUMENT_ROOT . "/template/footer.php");
 	?>		
 	</div>
 </body>
